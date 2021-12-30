cf1dfa7b440b0abae87358420a798b4b393091cc
squaremelon <iamjasper0828@gmail.com>
2021-12-30 12:37:45 +0800

    some changes

---
+++ 10100~10199/10114.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+struct Depreciation {
+    int month;
+    double percentage;
+};
+
+int main() {
+    int month, T;
+    float loan;
+    
+}
---
+++ 10800~10899/10805.cpp
@@ -0,0 +1,1 @@
+// Using Minimum Diameter Spanning Tree(MDST)
